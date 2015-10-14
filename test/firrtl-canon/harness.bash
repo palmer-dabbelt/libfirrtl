1916bd8cb5cf405a1c2df68df11de6b4ade1c38e
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2015-10-14 16:49:15 -0700

    Delete everything, I'm starting again

--- test/firrtl-canon/harness.bash
+++
@@ -1,3 +0,0 @@
-gdb --batch --ex r --ex bt --args $PTEST_BINARY --input Top.firx --output out.firx
-
-diff -u Top.firx out.firx
--- test/firrtl-canon/tempdir.bash
+++
@@ -1,5 +0,0 @@
-set -ex
-
-tempdir=`mktemp -d -t ptest-libfirrtl-canon.XXXXXXXXXX`
-trap "rm -rf $tempdir" EXIT
-cd $tempdir
